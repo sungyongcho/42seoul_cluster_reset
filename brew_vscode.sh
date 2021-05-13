1eae9e15a68edc35f536c1cffe5b4e9a341db08d
Sungyong Cho <to.sungyongcho@gmail.com>
2021-05-13 15:13:48 +0900

    brew_vscode updated

--- brew_vscode.sh
+++ brew_vscode.sh
@@ -6,8 +6,6 @@ rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME
 #visual studio code
 brew install visual-studio-code --appdir=~/Applications/
 
-echo 'export PATH="$PATH:/Users/sucho/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' >> $HOME/.zshrc
-
 code --install-extension kube.42header
 code --install-extension MariusvanWijk-JoppeKoers.codam-norminette-3
 code --install-extension ms-python.python
@@ -16,7 +14,10 @@ code --install-extension ms-vscode.cpptools
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension vscodevim.vim
 
-code ./vscode_settings/
+#copy and paster vscode settings
+cp ./vscode_settings/settings.json $HOME/Library/Application\ Support/Code/User/
+cp ./vscode_settings/keybindings.json $HOME/Library/Application\ Support/Code/User/
+
 
 echo "setting up brew & vscode in ~/Applications/ is DONE\n"
 
@@ -25,3 +26,6 @@ cat $HOME/.ssh/id_rsa_github.pub
 
 echo "ssh pubkey for 42 Intra\n\n"
 cat $HOME/.ssh/id_rsa_42intra.pub
+
+echo 'export PATH="$PATH:/Users/sucho/Applications/Visual Studio Code.app/Contents/Resources/app/bin"' >> $HOME/.zshrc
+
